0fe5ecff9d6168624e7c75a4aea345a09e8dca46
macknoyer <yudin.nc@gmail.com>
2019-11-21 12:40:07 +0300

    about.php

---
+++ about.php
@@ -0,0 +1,289 @@
+
+<?php include('templates/header.php');?>
+    
+    <!--====== PAGE BANNER PART START ======-->
+
+    <section class="page-banner bg_cover" style="background-image: url(assets/images/header2.jpg)" data-overlay="5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-content text-center">
+                        <h2 class="page-title">Professional Home Care Service in New York City</h2>
+                        <div class="page-subtitle">At Luba’s Home Care, we are committed to providing the best-specialized service. Learn more about us and lead a comfortable life with a caregiver you can trust.</div>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About Us</a></li>
+                        </ul>
+                    </div> <!-- page content -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== PAGE BANNER PART ENDS ======-->
+    
+
+    <section class="pt-80 pb-130">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title text-center pb-25">
+                        <h5 class="sub-title">Department</h5>
+                        <h3 class="title">About Us</h3>
+                    </div>
+                    <div>
+                        <p>Various circumstances might make daily tasks harder to fulfill, but that doesn't mean you have to go through such a challenging time alone. A dedicated caregiver will make your life easier in so many ways – personal care, domestic responsibilities, taking medication, or managing reduced mobility. At Luba’s Home Care, one of the best home care agencies in NYC, we offer a full range of home care services. With us, you get access to the unmatched quality of support, reassurance, and companionship. </p>
+                        <p>Home care is about trust. You may have a million questions – and rightly so. How do we balance giving the person some space but also make them feel safe at the same time? What if their personality doesn’t match the care provider? How do you know what the person needs?</p>
+                        <p>Our experience in home health care in New York City allows us to deal with all kinds of situations effortlessly. And we take the time to address any questions and concerns to gain your trust and build a long-lasting relationship.</p>
+                        <p>Our office is located in Brooklyn, but our service is available in all boroughs – Manhattan, Brooklyn, Queens, Staten Island, and The Bronx. Get in touch to arrange a booking or consultation. </p>
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <section class="pt-125 pb-130 gray-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mb-md-40">
+                    <img src="assets/images/about.jpg" alt="" class="img-cover">
+                </div>
+                <div class="col-md-6">
+                    <div class="section-title pb-25">
+                        <h5 class="sub-title">Department</h5>
+                        <h3 class="title">Our Mission</h3>
+                    </div>
+                    <div>   
+                        <p>Professional private home care agencies in NYC can set unreasonably high prices. It is important for us to make the service affordable – no one should be left behind when they need help the most. We don’t compromise quality for the sake of affordability in any way. Even though you want to make sure home healthcare doesn’t take a toll on your budget, doing our job to the highest standard remains our main priority. </p>
+                        <p>Our health care agency in New York gives you the flexibility to lead a normal, active life. To accommodate different needs, we provide a personal approach for every single client. After we discuss all the details, we will come up with the right schedule and a set of services, which will suit you personally. Care needs may also change, sometimes when you least expect it. Therefore, we are always open to discuss any adjustments.  </p>
+                        <p>Our efforts are dedicated to improving your life outside of your home, as well. You don’t have to give up your hobbies and interests because our providers will assist you in these ventures. If you feel confined in your home, it’s time to let yourself loose and enjoy your life to the fullest. </p>
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <section class="pt-125 pb-130">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title text-center pb-25">
+                        <h5 class="sub-title">Department</h5>
+                        <h3 class="title">When Would I Need Home Health Care in New York City?</h3>
+                    </div>
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="single-features  d-sm-flex mt-60">
+                        <div class="features-icon">
+                           <i class="la la-clinic-medical"></i>
+                        </div>
+                        <div class="features-content">
+                            <h4 class="features-title">Rehabilitation after illness or injury</h4>
+                            <p class="text">Recovery periods come with their own challenges. A caregiver will help manage certain tasks, so you can fully heal and get back to your normal health.</p>
+                            <!-- <a class="more" href="#">Read More <i class="la la-angle-right"></i></a> -->
+                        </div>
+                    </div> <!-- single features -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="single-features  d-sm-flex mt-60">
+                        <div class="features-icon">
+                          <i class="la la-heartbeat"></i>
+                        </div>
+                        <div class="features-content">
+                            <h4 class="features-title">Coping with a long-term illness and a chronic condition</h4>
+                            <p class="text">A caregiver will assist you with anything from managing your medication to moving around the house. </p>
+                            <!-- <a class="more" href="#">Read More <i class="la la-angle-right"></i></a> -->
+                        </div>
+                    </div> <!-- single features -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="single-features  d-sm-flex mt-60">
+                        <div class="features-icon">
+                           <i class="la la-hospital"></i>
+                        </div>
+                        <div class="features-content">
+                            <h4 class="features-title">Need for assistance with daily activities</h4>
+                            <p class="text">It might be hard to fulfill your regular responsibilities on your own, especially for seniors. Unfortunately, aging comes with certain struggles, so we want to help you with them. </p>
+                            <!-- <a class="more" href="#">Read More <i class="la la-angle-right"></i></a> -->
+                        </div>
+                    </div> <!-- single features -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="single-features  d-sm-flex mt-60">
+                        <div class="features-icon">
+                           <i class="la la-ambulance"></i>
+                        </div>
+                        <div class="features-content">
+                            <h4 class="features-title">A permanent caregiver is unavailable</h4>
+                            <p class="text">You never know when your caregiver might need a break. We will gladly fill in the position while they are away.</p>
+                            <!-- <a class="more" href="#">Read More <i class="la la-angle-right"></i></a> -->
+                        </div>
+                    </div> <!-- single features -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="mt-60">
+                        If you find yourself in one of these situations, we can help you. We provide the home care service in NY on a temporary or permanent basis. So, whether you need assistance for a specific period or long-term support, we will make arrangements for a caregiver depending on your specific needs. 
+                    </div> <!-- single features -->
+                </div>
+            </div>
+        </div> <!-- container -->
+    </section>
+    <section class="pt-125 pb-130 gray-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title text-center pb-25">
+                        <h5 class="sub-title">Department</h5>
+                        <h3 class="title">Does Your Home Health Agency in NYC Have Credible Providers?</h3>
+                    </div>
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="border_corners">
+                        We are adamant about our clients’ safety. We conduct an extensive vetting process for our care providers to ensure they are competent and trustworthy. This includes background checks, personal interviews, verification of credentials, and references from former employers.
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="border_corners">
+                        We guarantee that our New York City caregivers will provide the best possible service. It is our responsibility to make sure you feel comfortable, trusting our employees with your home and health.
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <section class="pt-125 pb-130">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title text-center pb-25">
+                        <h5 class="sub-title">Department</h5>
+                        <h3 class="title">Can I Afford Homecare in Manhattan?</h3>
+                    </div>
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="border_corners">
+                        There is a misconception that hiring private home care agencies in NYC is a luxury. When you need home assistance for a short period, it will not be a financial burden at all. You will use the service only for a limited time, and the benefit you get from it will fully justify the cost. 
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="border_corners">
+                        When it comes to senior long-term care, it is not largely different in price from assisted living facilities. And the fact that you can stay in the comfort of your home makes this arrangement even more appealing.
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <section class="pt-125 pb-130 gray-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title text-center pb-25">
+                        <h5 class="sub-title">Department</h5>
+                        <h3 class="title">24/7 Home Health Care Aides in NYC</h3>
+                    </div>
+                    <div>
+                        <p>We understand that home assistance is something people may need on a daily basis and at any point in the day. Our services are available 24 hours a day, 7 days a week. Whether it is the weekend or a holiday, we provide the service for your specific needs. </p>
+                        <p>Obtaining in-home services for yourself or a loved one is not an easy decision. But when you work with Luba’s Home Care, one of the best home care agencies in NYC, you can count on an impeccable experience. If you have any questions about our service, contact us – we are always happy to help</p>
+                    </div>
+                    <div class="about-content">
+                        <div class="about-form">
+                            <form action="#">
+                                <input type="text" placeholder="Enter Your Phone Number">
+                                <button class="main-btn">Make a call</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <!--====== TESTIMONIAL PART START ======-->
+
+    <section class="testimonial-two-area pt-125 pb-130">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="section-title">
+                        <h5 class="sub-title">Testimonials</h5>
+                        <h3 class="title">Service Recipient Says</h3>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="testimonial-two-active">
+                        <div class="single-testimonial-two mt-60">
+                            <div class="testimonial-content">
+                                <i class="la la-quotation"></i>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis turpis vitae tortor convallis eleifend. In volutpat maximus mauris non fringilla. Cras pulvinar aliquam massa nec lacinia.</p>
+                                <h5 class="testimonial-name">Patient</h5>
+                                <span>- Random Customer</span>
+                            </div> <!-- testimonial content -->
+                            <div class="testimonial-image">
+                                <img src="assets/images/testimonial2.jpg" alt="testimonial">
+                            </div>
+                        </div> <!-- single testimonial -->
+                        <div class="single-testimonial-two mt-60">
+                            <div class="testimonial-content">
+                                <i class="la la-quotation"></i>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis turpis vitae tortor convallis eleifend. In volutpat maximus mauris non fringilla. Cras pulvinar aliquam massa nec lacinia.</p>
+                                <h5 class="testimonial-name">Bobby Akter</h5>
+                                <span>- Random Customer</span>
+                            </div> <!-- testimonial content -->
+                            <div class="testimonial-image">
+                                <img src="assets/images/testimonial.png" alt="testimonial">
+                            </div>
+                        </div> <!-- single testimonial -->
+                        <div class="single-testimonial-two mt-60">
+                            <div class="testimonial-content">
+                                <i class="la la-quotation"></i>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis turpis vitae tortor convallis eleifend. In volutpat maximus mauris non fringilla. Cras pulvinar aliquam massa nec lacinia.</p>
+                                <h5 class="testimonial-name">Humayun Ahmed</h5>
+                                <span>- Random Customer</span>
+                            </div> <!-- testimonial content -->
+                            <div class="testimonial-image">
+                                <img src="assets/images/testimonial2.png" alt="testimonial">
+                            </div>
+                        </div> <!-- single testimonial -->
+                    </div> <!-- testimonial active -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== TESTIMONIAL PART ENDS ======-->
+
+    <!--====== CALL TO ACTION PART START ======-->
+
+    <section class="call-action-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="call-action d-lg-flex justify-content-between align-items-center">
+                        <div class="action-content">
+                            <h3 class="action-title">Don’t hasitate to contact us!</h3>
+                            <p class="text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem.</p>
+                        </div> <!-- action content -->
+                        <div class="action-btn">
+                            <a class="main-btn" href="#">get appointment</a>
+                        </div> <!-- action btn -->
+                    </div> <!-- call action -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== CALL TO ACTION PART ENDS ======-->
+
+<?php include('templates/footer.php');?>
