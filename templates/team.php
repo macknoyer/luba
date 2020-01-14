152d5c5ebe90dba03ca486b17139f0a09080fc92
macknoyer <yudin.nc@gmail.com>
2020-01-15 02:15:49 +0300

    fixes

--- templates/team.php
+++ templates/team.php
@@ -1,70 +1,17 @@
-
     <section class="doctors-two-are pt-125 pb-100">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-12">
-                    <div class="section-title text-center pb-25">
-                        <h5 class="sub-title">Expert Team</h5>
-                        <h3 class="title">Meet Our Team</h3>
-                    </div> <!-- section title -->
-                </div>
-            </div> <!-- row -->
-            <div class="row justify-content-center">
-                <div class="col-xl-12 col-lg-11 col-md-11 col-sm-10">
-                    <div class="row team-active">
-                        <div class="col-lg-4">
-                            <div class="single-team text-center mt-30 mb-30">
-                                <div class="team-image">
-                                    <img src="assets/images/team/t-7.jpg" alt="Team">
-                                </div>
-                                <div class="team-content">
-                                    <h5 class="team-name"><a href="doctors-details.html">Employee Name</a></h5>
-                                    <span>Founder</span>
-                                    <!-- <ul class="social">
-                                        <li><a href="#"><i class="la la-facebook-f"></i></a></li>
-                                        <li><a href="#"><i class="la la-twitter"></i></a></li>
-                                        <li><a href="#"><i class="la la-instagram"></i></a></li>
-                                        <li><a href="#"><i class="la la-linkedin"></i></a></li>
-                                    </ul> -->
-                                </div>
-                            </div> <!-- single team -->
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="single-team text-center mt-30 mb-30">
-                                <div class="team-image">
-                                    <img src="assets/images/team/t-8.jpg" alt="Team">
-                                </div>
-                                <div class="team-content">
-                                    <h5 class="team-name"><a href="doctors-details.html">Employee Name</a></h5>
-                                    <span>Nurse</span>
-                                   <!--  <ul class="social">
-                                        <li><a href="#"><i class="la la-facebook-f"></i></a></li>
-                                        <li><a href="#"><i class="la la-twitter"></i></a></li>
-                                        <li><a href="#"><i class="la la-instagram"></i></a></li>
-                                        <li><a href="#"><i class="la la-linkedin"></i></a></li>
-                                    </ul> -->
-                                </div>
-                            </div> <!-- single team -->
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="single-team text-center mt-30 mb-30">
-                                <div class="team-image">
-                                    <img src="assets/images/team/t-9.jpg" alt="Team">
-                                </div>
-                                <div class="team-content">
-                                    <h5 class="team-name"><a href="doctors-details.html">Employee Name</a></h5>
-                                    <span>Neurologist</span>
-                                    <!-- <ul class="social">
-                                        <li><a href="#"><i class="la la-facebook-f"></i></a></li>
-                                        <li><a href="#"><i class="la la-twitter"></i></a></li>
-                                        <li><a href="#"><i class="la la-instagram"></i></a></li>
-                                        <li><a href="#"><i class="la la-linkedin"></i></a></li>
-                                    </ul> -->
-                                </div>
-                            </div> <!-- single team -->
-                        </div>
-                    </div> <!-- row -->
-                </div>
-            </div> <!-- row -->
+            <div class="single-team">
+                <div class="row align-items-center">
+                    <div class="col-4"><div class="team-image"><img src="assets/images/luba.jpg" alt="" class="img-fluid"></div></div>
+                    <div class="col-8">
+                        <div class="section-title pb-25">
+                            <h5 class="sub-title">Founder</h5>
+                            <h3 class="title">Liubov Brizhatiuk</h3>
+                        </div> 
+                        <p><strong>Greetings. My name is Liubov Brizhatiuk.</strong> I'm administrating Home Care agencies in New York since 2000. I worked in the Home Health Care Services Industry where I started as a Staff Coordinator and advanced to Project Manager. I founded my own agency in 2018.</p>
+                        <p>Always welcome!</p>
+                    </div>
+                </div> <!-- row -->      
+            </div>
         </div> <!-- container -->
     </section>
