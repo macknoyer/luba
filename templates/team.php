58b00fd5f665e4fc8943e28cb4433a48f162947f
macknoyer <yudin.nc@gmail.com>
2020-01-15 17:24:30 +0300

    founder block

--- templates/team.php
+++ templates/team.php
@@ -2,9 +2,9 @@
         <div class="container">
             <div class="single-team">
                 <div class="row align-items-center">
-                    <div class="col-4"><div class="team-image"><img src="assets/images/luba.jpg" alt="" class="img-fluid"></div></div>
-                    <div class="col-8">
-                        <div class="section-title pb-25">
+                    <div class="col-md-4 d-flex justify-content-center"><div class="team-image mb-5 mb-md-0"><img src="assets/images/luba.jpg" alt="" class="img-fluid"></div></div>
+                    <div class="col-md-8">
+                        <div class="section-title text-center text-md-left pb-25">
                             <h5 class="sub-title">Founder</h5>
                             <h3 class="title">Liubov Brizhatiuk</h3>
                         </div> 
